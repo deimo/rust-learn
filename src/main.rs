0fc59d025fe8408d3987dfd525c95ec0dda428b4
劉　沖 <liucb@revolvesys.co.jp>
2024-10-31 17:00:16 +0900

    closure done

--- src/main.rs
+++ src/main.rs
@@ -24,19 +24,29 @@ fn main() {
     // println!("{:#?}", list);
     // println!("{list:#?}");
 
+    // let mut list = [
+    //     Rectangle { width: 10, height: 1 },
+    //     Rectangle { width: 3, height: 5 },
+    //     Rectangle { width: 7, height: 12 },
+    // ];
 
-    let mut list = [
-        Rectangle { width: 10, height: 1 },
-        Rectangle { width: 3, height: 5 },
-        Rectangle { width: 7, height: 12 },
-    ];
+    // let mut sort_operations = vec![];
+    // let value = String::from("closure called");
 
-    let mut sort_operations = vec![];
-    let value = String::from("closure called");
+    // list.sort_by_key(|r| {
+    //     sort_operations.push(value);
+    //     r.width
+    // });
+    // println!("{list:#?}");
 
-    list.sort_by_key(|r| {
-        sort_operations.push(value);
-        r.width
-    });
-    println!("{list:#?}");
+    let mut s = String::new();
+    let update_string =  |str| s.push_str(str);
+    exec(update_string);
+
+    println!("{:?}",s);
+
+}
+
+fn exec<'a, F: FnMut(&'a str)>(mut f: F) {
+    f("hello")
 }
---
+++ src/part1_closure/three_fn.rs
@@ -0,0 +1,93 @@
+pub fn test_fn(data: String) {
+    let fn_closure = |val| println!("{}", val);
+    fn_closure(data);
+}
+
+pub fn test_fn_mut(data: &mut String) {
+    let mut fnmut_closure = |val| println!("{:?}", data.push_str(val));
+    fnmut_closure("hhhh");
+}
+
+pub fn test_fn_once(data: String) {
+    let fnonce_closure = |val| {
+        println!("val: {}", val);
+        drop(data);
+    };
+    fnonce_closure("data");
+}
+
+pub fn foo() {
+    // 拿所有权
+    let s = String::new();
+    let update_string = move || println!("{}", s);
+
+    exec(update_string);
+    // exec(update_string);
+}
+
+pub fn bar() {
+    // 可变引用
+    let mut s = String::new();
+    let mut update_string = || s.push_str("hello");
+    exec(update_string);
+    // exec(update_string);
+}
+
+pub fn baz() {
+    let s = String::new();
+    let update_string = move || println!("{}", s);
+    exec(update_string);
+}
+
+pub fn demo() {
+    let s = String::new();
+
+    let update_string = || println!("{}", s);
+
+    exec(update_string);
+    exec1(update_string);
+    exec2(update_string);
+}
+
+pub fn demo2() {
+    let mut s = String::new();
+    let update_string = |str| -> String {
+        s.push_str(str);
+        s
+    };
+    exec4(update_string);
+}
+
+fn exec<F: FnOnce()>(f: F) {
+    f()
+}
+
+fn exec1<F: FnMut()>(mut f: F) {
+    f()
+}
+
+fn exec2<F: Fn()>(f: F) {
+    f()
+}
+
+fn exec4<'a, F: FnMut(&'a str) -> String>(mut f: F) {
+    f("hello");
+}
+
+fn factory(x: i32) -> impl Fn(i32) -> i32 {
+    let num = 5;
+
+    move |x| x + num
+}
+
+
+pub fn test() {
+    let mut s = String::new();
+    let update_string =  |str| s.push_str(str);
+    exec5(update_string);
+    println!("{:?}",s);
+}
+
+fn exec5<'a, F: FnMut(&'a str)>(mut f: F)  {
+    f("hello")
+}
